61ef9cc7fb1491be6f43db6321559d07d7a4797d
박서현 <s24005@gsm.hs.kr>
2025-02-05 21:22:23 +0900

    tailwind 이미지 넣기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
    <>
     <div className="max-w-64 h-20 bg-blue-400 rounded-xl m-60 mt-10">
+      <div className="bg-[url('https://your-image-url.com/image.png')] bg-cover bg-center"></div>
       <span className="font-mono text-base ml-16 mb-8-7 text-yellow-50">ChitChat</span>
       <p className="text-xs text-gray-300 ml-16">You have a new message!</p>
     </div>
